7f3d0a0e3b4b90621c31ace54f219fe9cbee9428
AllineedRu <allineed.ru@gmail.com>
2022-12-03 21:14:08 +0300

    Added license text to the header

--- allineed-core/src/main/java/ru/allineed/samples/io/BufferedReaderWithCycle.java
+++ allineed-core/src/main/java/ru/allineed/samples/io/BufferedReaderWithCycle.java
@@ -1,3 +1,20 @@
+/**
+ * [RU] Класс с примером Java кода из статьи
+ * https://allineed.ru/development/java-development/22-java-reading-data-from-file
+ *
+ * [EN] The class with sample Java code from the article
+ * https://allineed.ru/development/java-development/22-java-reading-data-from-file
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package ru.allineed.samples.io;
 
 import java.io.BufferedReader;
